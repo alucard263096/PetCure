24c094e8b3a14dcbcaeadaff103aa0159e58fc87
steve.cai <alucard263096@126.com>
2016-05-09 01:22:53 +0800

    加上测试URL

--- API/Poster/create.php
+++ API/Poster/create.php
@@ -1,5 +1,6 @@
 <?php
   require '../include/common.inc.php';
+  //view-source:http://localhost/petcure/api/Poster/create.php?a=c&photos=9757de3ee5369c184891e30e39a1b29e.jpg%3Bf408110930b8450a5433a2af151f6049.jpg%3B&address=%E5%B9%BF%E4%B8%9C%E7%9C%81%E6%B7%B1%E5%9C%B3%E5%B8%82%E9%BE%99%E5%B2%97%E5%8C%BA%E5%BB%BA%E6%96%B0%E8%B7%AF8%E5%8F%B7&needs=%E7%9C%8B%E8%A7%81%E4%BA%86&lng=114.275334&type=0&contact=1375108256&lat=22.724631&member_id=1
   outputXml(outResult(0,"update success",0));
 
 ?>
