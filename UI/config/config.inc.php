271da5dc7097f9683c41b2d5827ccf5cc55b90ea
steve.cai <alucard263096@126.com>
2016-05-13 01:13:45 +0800

    fix得差不多，有bug明天改

--- UI/config/config.inc.php
+++ UI/config/config.inc.php
@@ -27,7 +27,7 @@ $CONFIG['error_handler'] ="E_ALL";
 
 #[Database]
 $CONFIG['database']['provider']	= 'mysql';  //mssql,sqlsrv
-$CONFIG['database']['host']		= 'localhost';  
+$CONFIG['database']['host']		= 'www.myhkdoc.com';  
 $CONFIG['database']['database']	= 'cw_petcure';  
 $CONFIG['database']['user']		= 'root';  
 $CONFIG['database']['psw']		= 'root'; 
