f83a21fcfa59ff644f22501d8c3c6a7317b096e1
Steve Cai <steve.cai@morningstar.com>
2016-05-17 15:58:47 +0800

    fix

--- API/Poster/list.php
+++ API/Poster/list.php
@@ -1,6 +1,6 @@
 <?php
   require '../include/common.inc.php';
   include ROOT.'/classes/datamgr/poster.cls.php';
-  $result=$posterMgr->getPosterList($_REQUEST["lat"],$_REQUEST["lng"],$_REQUEST["page"],$_REQUEST["count"]);
+  $result=$posterMgr->getPosterList($_REQUEST["lat"],$_REQUEST["lng"],$_REQUEST["page"],$_REQUEST["count"],$_REQUEST["type"],$_REQUEST["order"]);
   outputXml($result);
 ?>
