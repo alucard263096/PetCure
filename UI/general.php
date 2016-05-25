984e5e36acfbeea037f1793d5abf987e173df9ee
Steve Cai <steve.cai@morningstar.com>
2016-05-25 15:53:00 +0800

    完成generaltext的显示

---
+++ UI/general.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Created on 2012-6-30
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+  require 'include/common.inc.php';
+  include ROOT.'/include/init.inc.php';
+
+  include ROOT.'/classes/mgr/'.$CONFIG['database']['provider'].'.cls.php';
+  require ROOT.'/classes/datamgr/general.cls.php';
+  
+  $content=$generalMgr->getContent($_REQUEST["code"]);
+  $smarty->assign("content",$content);
+
+  
+  $smarty->assign("subform","Y");
+  
+  $smarty->display(ROOT.'/templates/general.html');
+?>
--- UI/include/init.inc.php
+++ UI/include/init.inc.php
@@ -2,6 +2,7 @@
 
 
  $smarty->assign('MapKey',$CONFIG['Tencent']["MapKey"]);
+
  
   $member=$_SESSION[SESSIONNAME]["member"];
   if($member!=null&&$member["id"]!=""){
@@ -18,6 +19,33 @@
 	$smarty->assign("member",$member);
   }
 
+  
+ $thisurl=$_SESSION[SESSIONNAME]["thisurl"];
+ $lasturl=$_SESSION[SESSIONNAME]["lasturl"];
 
+ if($thisurl==""){
+	$_SESSION[SESSIONNAME]["thisurl"]=$_REQUEST["REQUEST_URI"];
+	$thisurl=$_REQUEST["REQUEST_URI"];
+ }
+ if($lasturl==""){
+	$_SESSION[SESSIONNAME]["lasturl"]=$CONFIG['URL']."/index.php";
+	$lasturl=$CONFIG['URL']."/index.php";
+ }
+ 
+ $_SESSION[SESSIONNAME]["lasturl"]=$thisurl;
+ $_SESSION[SESSIONNAME]["thisurl"]=$_REQUEST["REQUEST_URI"];
+
+ 
+ $thisurl=$_SESSION[SESSIONNAME]["thisurl"];
+ $lasturl=$_SESSION[SESSIONNAME]["lasturl"];
+
+ if($thisurl==$lasturl){
+	$_SESSION[SESSIONNAME]["lasturl"]=$CONFIG['URL']."/index.php";
+	$lasturl=$CONFIG['URL']."/index.php";
+ }
+
+ $smarty->assign("thisurl",$thisurl);
+ $smarty->assign("lasturl",$lasturl);
+ 
 
 ?>
