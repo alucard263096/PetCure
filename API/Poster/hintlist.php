240c01a02d746d9d6b1e046d438bc8df3c0bb10d
steve.cai <alucard263096@126.com>
2016-05-26 23:22:26 +0800

    嵌入代码

--- API/Poster/hintlist.php
+++ API/Poster/hintlist.php
@@ -1,6 +1,6 @@
 <?php
   require '../include/common.inc.php';
   include ROOT.'/classes/datamgr/poster.cls.php';
-  $result=$posterMgr->hintList($_REQUEST["poster_id"]);
+  $result=$posterMgr->hintList($_REQUEST["poster_id"],$_REQUEST["page"],$_REQUEST["count"]);
   outputXml($result);
 ?>
